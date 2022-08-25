59cf28b8cb4cd175712fcae82d0c12e18f91dcd1
Daniel Matias <91097480+matiasdanielz@users.noreply.github.com>
2022-08-25 14:42:19 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,10 +3,4 @@ from numbersController import DiretorioDeFuncoes
 if __name__ == '__main__':
 
     controleDeNumeros = DiretorioDeFuncoes.validacaoDeNumeros()
-
-    opcao = int(input('Escolha uma opcao: '))
-
-    if(opcao == 1):
-        controleDeNumeros.RetornarSomaDeValores()
-    else:
-        controleDeNumeros.VerificaSenumeroEPar()
+    controleDeNumeros.BuscaDeMenorNumeroEmArray()
--- numbersController/DiretorioDeFuncoes.py
+++ numbersController/DiretorioDeFuncoes.py
@@ -1,6 +1,6 @@
 class validacaoDeNumeros:
 
-    def VerificaSenumeroEPar(self):
+    def VerificaSeNumeroEPar(self):
         numeroEscolhido = int(input('Escolha um numero: '))
         resultado = numeroEscolhido % 2
 
@@ -10,9 +10,19 @@ class validacaoDeNumeros:
             print('numero é par')
 
 
-    def RetornarSomaDeValores(self):
+    def SomarValores(self):
+
         Primeironumero = int(input('Digite um numero: '))
         SegundoNumero = int(input('Digite o segundo numero: '))
+
         soma = Primeironumero + SegundoNumero
         print(f'A soma dos valores e: {soma}')
 
+    def BuscaDeMenorNumeroEmArray(self):
+        listaDeNumeros = []
+
+        for i in range(4):
+            numero = int(input(f'Digite o {i + 1}º numero: '))
+            listaDeNumeros.append(numero)
+        listaDeNumeros.sort()
+        print(listaDeNumeros[0])
